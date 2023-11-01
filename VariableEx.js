06fae3979152b6eaba7344553cdfd56e61c24edf
lizaranisamal <lizaranisamal1@gmail.com>
2023-11-01 08:02:06 +0530

    variable example learnng

---
+++ VariableEx.js
@@ -0,0 +1,44 @@
+let pens = 10;
+let pencile = 5;
+console.log(pens);
+console.log(pencile);
+let total = pens + pencile;
+console.log(total);
+// pens = 25;
+console.log(pens);
+pens = pens + 15;
+console.log(pens);
+
+
+
+
+let score = 0;
+score = score + 5;
+console.log(score);
+score += 5;
+console.log(score);
+console.log(score);
+
+
+let num = 10;
+// num++
+console.log(num++);
+console.log(num);
+num--;
+console.log(num);
+++num
+ console.log(num);
+
+
+ const num1 = 26;
+ console.log(num1);
+ //num1 +=1;
+ console.log(num1);
+
+
+
+
+ var runDistance = 26.2;
+ console.log(runDistance);
+ runDistance += 1;
+ console.log(runDistance);
---
+++ firstprogram.js
@@ -0,0 +1,46 @@
+// console.log(2+3)
+// console.log(6/2)
+// console.log(6**2)   // **=power sign 
+// console.log(19%4)
+// console.log((2*3)+5)
+// console.log(0/0)    //NaN = not a number
+// console.log(typeof 4)
+// console.log(200+0/0)
+// console.log(4+3*4/2)
+// console.log(4+3)
+
+
+
+
+
+
+
+
+let a = 10;
+let b = 10.5;
+let c = 'a';
+let str = "liza";
+let g = true;
+let arr = [1,2,3];
+console.log(a+b);
+console.log(typeof(c));
+console.log(str.length);
+console.log(arr.length);
+console.log(arr[0]);
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    console.log(element);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
