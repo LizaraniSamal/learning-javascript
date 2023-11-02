39adf8841677cfd6e1be86f8299bce4d8bfe9ef4
lizaranisamal <lizaranisamal1@gmail.com>
2023-11-02 19:54:26 +0530

    learning array javascript

---
+++ array.js
@@ -0,0 +1,62 @@
+console.log("hello world");
+// let username = prompt("enter your name");
+// if(username.length >= 6){
+//     console.log("valid name");
+// }
+
+let days = ['sunday', 'monday', 'tuesday'];
+console.log(days);
+let colors = ['red','orange','blue','golden','sky','violet'];
+console.log(colors);
+console.log(colors[3]);
+console.log(colors[1][2]);
+colors[2] = 'navy blue';
+console.log(colors);
+
+
+//ARRAY METHODS
+// Push - add to end
+// pop - remove from end
+// shift - remove from Start
+// unshift - add to start
+let num = [15,18,17,16];
+num[4] = 25;
+console.log(num);
+num.push(30);
+console.log(num);
+num.push(45,58);
+console.log(num);
+num.pop();
+console.log(num);
+let newNumber = num.shift();
+console.log(newNumber);
+console.log(num);
+num.unshift(80);
+console.log(num);
+
+
+
+//MORE METHODS
+  let userName = ['liza','puja','lucky','pupun'];
+  let age = [23,25,15,22];
+  total = userName.concat(age);
+  console.log(total);
+console.log(userName.includes('liza'));   //true
+console.log(userName.includes('Liza'));  //false
+console.log(userName.reverse());
+console.log(total.reverse());
+
+let coolColors = ['red','orange','blue','golden','sky','violet','yellow','green'];
+console.log(coolColors.slice(3,5));
+console.log(coolColors.slice(-3));
+
+let months = ['jan','feb','mar','apl','may'];
+remainMonth = months.splice(1,4);
+console.log(months);
+
+
+//multi dimensional array
+const game = [['x','o','x'], ['o','x','o'], ['x',null, 'x']];
+console.log(game);
+console.log(game[2][1]);
+console.log(game[1][0]);
--- decisionMaking.js
+++ decisionMaking.js
@@ -14,6 +14,31 @@ console.log(1==='1');       //=== strict equal
 console.log(1 != '1');
 console.log(1 !== '1');
 
-let message = prompt("enter a no");
-console.log(message);
+// let message = prompt("enter a no");
+// console.log(message);
+
+if(1+1 == 2){
+    console.log("the above statement is true");
+}
+
+// let random = math.random(); 
+// if(random < 0.5){
+//     console.log("your number is less than 0.5");
+//     console.log(random);
+// }
+
+
+// if else
+let Mage = 25;
+if(Mage == 20){
+    console.log("you are adult");
+}
+else if(Mage == 25){
+    console.log(" you are also adult");
+}
+else{
+    console.log("you are child");
+}
+
+
 
