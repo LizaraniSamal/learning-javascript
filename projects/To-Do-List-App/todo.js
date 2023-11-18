ecd75c05274de6fc0f2c702d26caa118fe56d17e
lizaranisamal <lizaranisamal1@gmail.com>
2023-11-18 20:06:20 +0530

    to-do list application done

--- projects/To-Do-List-App/todo.js
+++ projects/To-Do-List-App/todo.js
@@ -1,8 +1,7 @@
 
-
+const listContainer = document.getElementById("list-container");
 function addTask(){
     const inputBox = document.getElementById("input-box");
-    const ulList = document.getElementById("list-container");
     console.log("hello",inputBox);
     if(inputBox.value === ""){
         alert("you must write something!");
@@ -10,7 +9,32 @@ function addTask(){
     else{
         const li = document.createElement("li");
         li.innerHTML = inputBox.value;
-        ulList.appendChild(li);
-        
+        listContainer.appendChild(li);
+        inputBox.value="";
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
+    saveData();
 }
+
+listContainer.addEventListener("click" ,function (e){
+    console.log(e.target.tagName);
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
